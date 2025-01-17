ed0b29712c28ebd983d980226cdcdeb68d4fab47
santosh-eic <santosh.pagire@einfochips.com>
2025-01-17 17:18:26 +0530

    pagination added

--- application_tagging.py
+++ application_tagging.py
@@ -2,16 +2,16 @@ import boto3
 import os
 
 # Exported Environment variables
-REGION = os.getenv('AWS_REGION')
+AWS_REGION = os.getenv('AWS_REGION')
 AWS_APPLICATION_ARN = os.getenv('AWS_APPLICATION_ARN')
 TAG_KEY = os.getenv('TAG_KEY')
 TAG_VALUE = os.getenv('TAG_VALUE')
 
 
 def list_and_tag_resources():
-    print(f"Checking resources in region: {REGION}")
+    print(f"Checking resources in region: {AWS_REGION}")
     
-    session = boto3.Session(region_name=REGION)
+    session = boto3.Session(region_name=AWS_REGION)
     resource_client = session.client('resourcegroupstaggingapi')
 
     tag_filters = {
@@ -23,37 +23,45 @@ def list_and_tag_resources():
         ]
     }
 
-    # Get resources with the specified tag
-    response = resource_client.get_resources(**tag_filters)
-
-    resources = response.get('ResourceTagMappingList', [])
     tagged_resources = 0
+    pagination_token = None
 
-    for resource in resources:
-        resource_arn = resource['ResourceARN']
-        tags = {tag['Key']: tag['Value'] for tag in resource['Tags']}
-        resource_type = resource_arn.split(':')[5] 
-        region = resource_arn.split(':')[3]  
+    while True:
+        if pagination_token:
+            tag_filters['PaginationToken'] = pagination_token
 
-        # Check if 'awsApplication' tag is present
-        if 'awsApplication' not in tags:
-            try:
-                resource_client.tag_resources(
-                    ResourceARNList=[resource_arn],
-                    Tags={'awsApplication': AWS_APPLICATION_ARN}
-                )
-                tagged_resources += 1
-                print(f"Successfully tagged resource: {resource_arn}")
-            except Exception as e:
-                print(f"Failed to tag resource: {resource_arn}. Error: {str(e)}")
-        else:
-            print(f"Resource already tagged: {resource_arn}")
+        # Get resources with the specified tag
+        response = resource_client.get_resources(**tag_filters)
 
-   
+        resources = response.get('ResourceTagMappingList', [])
+
+        for resource in resources:
+            resource_arn = resource['ResourceARN']
+            tags = {tag['Key']: tag['Value'] for tag in resource['Tags']}
+
+            # Check if 'awsApplication' tag is present
+            if 'awsApplication' not in tags:
+                try:
+                    resource_client.tag_resources(
+                        ResourceARNList=[resource_arn],
+                        Tags={'awsApplication': AWS_APPLICATION_ARN}
+                    )
+                    tagged_resources += 1
+                    print(f"Successfully tagged resource: {resource_arn}")
+                except Exception as e:
+                    print(f"Failed to tag resource: {resource_arn}. Error: {str(e)}")
+            else:
+                print(f"Resource already tagged: {resource_arn}")
+
+        # Check if there are more resources to fetch
+        pagination_token = response.get('PaginationToken')
+        if not pagination_token:
+            break
+
     if tagged_resources > 0:
-        print(f"Tagging completed successfully in {REGION}. Tagged {tagged_resources} resources.")
+        print(f"Tagging completed successfully in {AWS_REGION}. Tagged {tagged_resources} resources.")
     else:
-        print(f"No resources were tagged in {REGION}.")
+        print(f"No resources were tagged in {AWS_REGION}.")
 
 if __name__ == "__main__":
     list_and_tag_resources()
